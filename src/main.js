f5a2a60ced64357618c8ece2a779a7abbcdea353
faisalhatila <hatilas1994@gmail.com>
2021-02-09 18:52:51 +0500

    1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,58 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' }, // our-domain.com/teams => TeamsList
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        } // our-domain.com/teams => TeamsList
+      ]
+    }, // our-domain.com/teams => TeamsList
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+
+    { path: '/:notFound(.*)', redirect: '/teams' } // our-domain.com/teams => TeamsList
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    // console.log('to', to);
+    // console.log('from', from);
+    // console.log('savedPosition', savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return {
+      left: 0,
+      top: 0
+    };
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('Global BeforeEach');
+  console.log('to', to);
+  console.log('from', from);
+  next(true);
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
